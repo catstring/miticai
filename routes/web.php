e113cf78bb4583f7dff8b1ea596d81c8112e87bd
Todd MC Tsai <toddmctsai@gmail.com>
2024-09-13 21:31:35 +0800

    google auth setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('auth/google', [AuthController::class, 'redirectToGoogle']);
+Route::get('auth/google/callback', [AuthController::class, 'handleGoogleCallback']);
+Route::post('auth/logout', [AuthController::class, 'logout'])->name('logout');
